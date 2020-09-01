e6e0fc574a010f7b707f0cfc1ca37d7e697f6e59
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-01 06:55:28 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -45,5 +45,6 @@ def add_characters
          ]
       }
    }
-   epic_tragedy[:additional_characters] = [{:name => "Prince Escalus"},{:name => "Apothecary" }]
+   epic_tragedy[:additional_characters] 
+   epic_tragedy[:additional_characters]= [{:name => "Prince Escalus"},{:name => "Apothecary" }]
 end
